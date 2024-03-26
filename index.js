c1b0ef629d853ed14a75f9e3ae27436b1911fe8b
Yves Jaques <ymoraisjaques@gmail.com>
2024-03-25 21:58:57 -0300

    Pagination & Saving items on csv

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const puppeteer = require('puppeteer');
 
 (async () => {
@@ -7,43 +8,67 @@ const puppeteer = require('puppeteer');
     userDataDir: "./tmp"
   });
   const page = await browser.newPage();
-  await page.goto('https://www.amazon.com/s?k=laptop&crid=3WY0EB14VVIN&sprefix=laptop%2Caps%2C231&ref=nb_sb_noss_1');
+  const url = 'https://www.amazon.com/s?k=laptop+128gb&i=electronics&rh=n%3A172282%2Cp_123%3A241862&dc&language=en_US&ds=v1%3AQf4hE5jc2PMpmmohWn6bMDhJLQeKDnv4Shv0giS10aI&crid=3DPWGAPH4CMSK&currency=BRL&qid=1711412103&rnid=85457740011&sprefix=laptop+128gb%2Caps%2C228&ref=sr_nr_p_123_5'
+  await page.goto(url);
 
-  const productsHandles = await page.$$('div.s-main-slot.s-result-list.s-search-results.sg-row > .s-result-item')
+  let isBtnDisabled = false;
+  while (!isBtnDisabled) {
+    await page.waitForSelector('[data-cel-widget="search_result_0"]')
 
-  let items = [];
+    const itemSelector = 'div.s-main-slot.s-result-list.s-search-results.sg-row > .s-result-item'
+    const productsHandles = await page.$$(itemSelector)
 
-  for (const productHandle of productsHandles) {
-    try {
-      const title = await page.evaluate(
-        el => el.querySelector('h2 > a > span')?.textContent || '',
-        productHandle
-      )
+    for (const productHandle of productsHandles) {
+      try {
+        const title = await page.evaluate(
+          el => el.querySelector('h2 > a > span')?.textContent || '',
+          productHandle
+        )
 
-      const price = await page.evaluate(
-        el => el.querySelector('.a-price > .a-offscreen')?.textContent || '',
-        productHandle
-      )
+        const price = await page.evaluate(
+          el => el.querySelector('.a-price > .a-offscreen')?.textContent || '',
+          productHandle
+        )
 
-      const img = await page.evaluate(
-        el => el.querySelector('.s-image')?.getAttribute('src') || '',
-        productHandle
-      )
+        const img = await page.evaluate(
+          el => el.querySelector('.s-image')?.getAttribute('src') || '',
+          productHandle
+        )
 
-      if (title && price && img) {
-        const item = {
-          title,
-          price,
-          img
+        if (title && price && img) {
+          const item = {
+            title,
+            price,
+            img
+          }
+          fs.appendFile(
+            'results.csv',
+            `${title.replace(/,/g, ".")}, ${price}, ${img}\n`,
+            function (err) {
+              if (err) throw err;
+            }
+          )
         }
-        items.push(item)
+
+      } catch (error) {
+
       }
-    } catch (error) {
-      
+    }
+
+    const selector = '.s-pagination-next'
+    const disabledSelector = '.s-pagination-next.s-pagination-disabled'
+
+    await page.waitForSelector(selector, { visible: true })
+    const is_disabled = await page.$(disabledSelector) !== null
+
+    isBtnDisabled = is_disabled
+    if (!is_disabled) {
+      await Promise.all([
+        page.click(selector),
+        page.waitForNavigation({ waitUntil: "networkidle2" })
+      ])
     }
   }
-  
-  console.log(items.length)
-  // console.log(items)
-  // await browser.close();
+
+  await browser.close();
 })();
