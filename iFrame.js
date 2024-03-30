4aeff382595b29d2dcc3ea47ccd1a13d990b38f7
Yves Jaques <ymoraisjaques@gmail.com>
2024-03-30 10:34:51 -0300

    Getting content inside iFrame

---
+++ iFrame.js
@@ -0,0 +1,18 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch({
+      headless: false,
+    });
+    const page = await browser.newPage();
+    const url = 'https://developer.mozilla.org/en-US/docs/Web/HTML/Element/iframe'
+    await page.goto(url);
+
+    const iFrame1st = await page.$("#content > article > section:nth-child(3) > div > iframe")
+    
+    const iFrame1stContentFrame = await iFrame1st.contentFrame();
+
+    const title = await iFrame1stContentFrame.$eval('body > header > h4', (el) => el.innerText)
+
+    console.log(title);
+})();
